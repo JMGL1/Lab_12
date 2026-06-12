9e5ecf778bee93dc408a9a089ce1e16566340d37
nacho <joseignaciogallardolaura995@gmail.com>
2026-06-12 09:48:06 -0400

    cambios esteticos

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const api = axios.create({
   //baseURL: 'http://localhost:3001',
   baseURL: '/api',
-  timeout: 10000,
+  timeout: 60000,
   headers: { 'Content-Type': 'application/json' }
 });
 
